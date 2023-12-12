74783cb3be41cb8f071ebb6bdaa152b736cd2ebc
Caleb <cjeckerley@gmail.com>
2023-12-12 18:11:46 -0500

    Navigation Bar now accepts a list of menu items as a prop. Created Experience Plate to display work history

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,30 +1,44 @@
-export default function NavigationBar() {
+function generateNavItems(navList) {
+  let navigationList = [];
+  for (let idx = 0; idx < navList.length; idx++) {
+    let navObj = navList[idx];
+
+    navigationList.push(
+      <li style={{ position: "relative", boxSizing: "border-box" }} key={idx}>
+        <a href={navObj.href} style={{ background: "none", cursor: "pointer", textDecoration: "none", color: "white", padding: "0.5rem" }}>
+          {navObj.text}
+        </a>
+      </li>
+    );
+  }
+  return navigationList;
+}
+
+export default function NavigationBar({ navList }) {
+  const navMembers = generateNavItems(navList);
+
   return (
-    <nav>
-      <ul
-        style={{
-          width: "100%",
-          listStyle: "none",
-          padding: 0,
-          margin: 0,
-          display: "flex",
-          gap: "1rem",
-          justifyContent: "space-around",
-          alignItems: "center",
-          boxSizing: "border-box"
-        }}
-      >
-        <li style={{ position: "relative", boxSizing: "border-box" }}>
-          <a href='' style={{ background: "none", cursor: "pointer", textDecoration: "none", color: "white", padding: "0.5rem" }}>
-            Resume
-          </a>
-        </li>
-        <li>
-          <a href='' style={{ background: "none", cursor: "pointer", textDecoration: "none", color: "white" }}>
-            Projects
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <hr style={{ marginBottom: "0px", marginTop: "0px" }} />
+      <nav>
+        <ul
+          style={{
+            height: "100%",
+            width: "100%",
+            listStyle: "none",
+            padding: 0,
+            margin: 0,
+            display: "flex",
+            gap: "1rem",
+            justifyContent: "space-around",
+            alignItems: "center",
+            boxSizing: "border-box"
+          }}
+        >
+          {navMembers}
+        </ul>
+      </nav>
+      <hr style={{ marginBottom: "0px", marginTop: "0px" }} />
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 //Node imports
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap-icons/font/bootstrap-icons.scss";
+// import "bootstrap-icons/font/bootstrap-icons.scss";
 
 //Font imports
+import "@fontsource/roboto/100.css";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
 
 //Style imports
 import "/src/style/index.scss";
@@ -18,16 +20,32 @@ import Header from "/src/Header.jsx";
 import AboutContainer from "/src/AboutContainer.jsx";
 import IconLink from "/src/components/IconLink.jsx";
 import NavigationBar from "/src/components/NavigationBar.jsx";
+import ExperiencePlate from "/src/components/ExperiencePlate.jsx";
 
 //Dummy data import
 import jsonData from "../data/about-me.json";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AboutContainer data={jsonData} />
-    <hr style={{ marginBottom: "0px" }} />
-    <NavigationBar />
-    <hr style={{ marginTop: "0px" }} />
+    <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+      <AboutContainer data={jsonData} />
+      <NavigationBar
+        navList={[
+          { text: "Work", href: "" },
+          { text: "Projects", href: "" },
+          { text: "Skills", href: "" }
+        ]}
+      />
+      <div style={{ paddingTop: "1rem" }}>
+        <ExperiencePlate
+          jobTitleProp={"NetSuite Developer"}
+          employerProp={"Solution Source"}
+          jobStartProp={"September 1, 2022"}
+          jobEndProp={"July 31, 2023"}
+          contentListProp={Array(15).fill("hello", 0, 15)}
+        />
+      </div>
+    </div>
     {/* <IconLink iconClass='bi-house' text='testing testing 123 testing' /> */}
   </React.StrictMode>
 );
