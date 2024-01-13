0b644ffdce3caeb1d4a098729baab2b869e91626
Caleb <cjeckerley@gmail.com>
2024-01-12 21:35:26 -0500

    First pass at skill list

--- src/components/SkillsList.jsx
+++ src/components/SkillsList.jsx
@@ -1,24 +1,45 @@
 import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
 
 export default function SkillsList(props) {
   if (props.isHidden) {
     return null;
   }
 
-  const chips = props.data.map((chip) => {
-    return (
+  const chipStyleRule = {
+    Software: { background: "green", color: "white" },
+    "Data Science": { background: "blue", color: "white" },
+    DevOps: { background: "red", color: "white" },
+    General: { background: "grey", color: "white" }
+  };
+
+  let categories = { Software: [], "Data Science": [], DevOps: [], General: [] };
+
+  props.data.forEach((chip) => {
+    categories[chip.type].push(
       <Chip
         key={chip._id}
         label={chip.name}
         size='small'
         style={{
-          background: "lightblue",
           paddingTop: 0,
           width: "fit-content"
         }}
+        sx={chipStyleRule[chip.type]}
       />
     );
   });
 
-  return <>{chips}</>;
+  const skillList = Object.entries(categories).map(([key, value]) => {
+    return (
+      <div key={key} style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+        <Divider orientation='horizontal' variant='middle'>
+          {`${key} Skills`}
+        </Divider>
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "0.25rem" }}>{value}</div>
+      </div>
+    );
+  });
+
+  return <>{skillList}</>;
 }
